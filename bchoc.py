e3541a8604dfba75ef9cffd9d9396f6ab1c604dc
Gus Fowler <gus.fowler12@gmail.com>
2022-04-25 17:22:15 -0700

    init

---
+++ bchoc.py
@@ -0,0 +1,23 @@
+import os
+import uuid
+import argparse
+import struct
+
+class blockchain:
+    def __init__(self, filePath):
+        self.filePath = filePath
+
+    def initiate(self):
+        ###
+
+    def insert(self):
+        ###
+    
+    def checkout(self):
+        ###
+
+    def checkin(self):
+        ###
+
+    def remove(self):
+        ###
